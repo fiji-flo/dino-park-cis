397fdbb4754985d1c557b436a78fab4ce2c340ea
Florian Merz <me@fiji-flo.de>
2020-06-30 13:26:39 +0200

    publishers

--- src/profile/publishers.rs
+++ src/profile/publishers.rs
@@ -2,18 +2,26 @@ use chrono::DateTime;
 use chrono::Duration;
 use chrono::Utc;
 use cis_profile::schema::PublisherAuthority;
+use failure::Error;
 use futures::FutureExt;
 use futures::TryFutureExt;
 use headers::CacheControl;
 use headers::HeaderMapExt;
 use lazy_static::lazy_static;
+use serde::de;
+use serde::de::MapAccess;
+use serde::de::Visitor;
 use serde::Deserialize;
+use serde::Deserializer;
 use serde::Serialize;
 use shared_expiry_get::Expiry;
 use shared_expiry_get::ExpiryFut;
 use shared_expiry_get::ExpiryGetError;
 use shared_expiry_get::Provider;
 use shared_expiry_get::RemoteStore;
+use std::fmt;
+use std::marker::PhantomData;
+use std::str::FromStr;
 
 lazy_static! {
     pub static ref PUBLISHER_RULES: RemoteStore<RemotePublisherRules, RemotePublisherRulesProvider> =
@@ -113,64 +121,162 @@ pub struct Rules {
     pub uris: Publishers,
     pub phone_numbers: Publishers,
 
+    #[serde(deserialize_with = "string_or_struct")]
     pub identities: IdentityRules,
     pub access_information: AccessInformationRules,
+    #[serde(deserialize_with = "string_or_struct")]
     pub staff_information: StaffInformationRules,
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
-#[serde(untagged)]
-pub enum IdentityRules {
-    Different {
-        github_id_v3: Publishers,
-        github_id_v4: Publishers,
-        github_primary_email: Publishers,
-        mozilliansorg_id: Publishers,
-        bugzilla_mozilla_org_id: Publishers,
-        bugzilla_mozilla_org_primary_email: Publishers,
-        mozilla_ldap_id: Publishers,
-        mozilla_ldap_primary_email: Publishers,
-        mozilla_posix_id: Publishers,
-        google_oauth2_id: Publishers,
-        google_primary_email: Publishers,
-        firefox_accounts_id: Publishers,
-        firefox_accounts_primary_email: Publishers,
-        custom_1_primary_email: Publishers,
-        custom_2_primary_email: Publishers,
-        custom_3_primary_email: Publishers,
-    },
-    Same(Publishers),
+pub struct IdentityRules {
+    pub github_id_v3: Publishers,
+    pub github_id_v4: Publishers,
+    pub github_primary_email: Publishers,
+    pub mozilliansorg_id: Publishers,
+    pub bugzilla_mozilla_org_id: Publishers,
+    pub bugzilla_mozilla_org_primary_email: Publishers,
+    pub mozilla_ldap_id: Publishers,
+    pub mozilla_ldap_primary_email: Publishers,
+    pub mozilla_posix_id: Publishers,
+    pub google_oauth2_id: Publishers,
+    pub google_primary_email: Publishers,
+    pub firefox_accounts_id: Publishers,
+    pub firefox_accounts_primary_email: Publishers,
+    pub custom_1_primary_email: Publishers,
+    pub custom_2_primary_email: Publishers,
+    pub custom_3_primary_email: Publishers,
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
-#[serde(untagged)]
-pub enum AccessInformationRules {
-    Different {
-        access_provider: Publishers,
-        ldap: Publishers,
-        hris: Publishers,
-        mozilliansorg: Publishers,
-    },
-    Same(Publishers),
+pub struct AccessInformationRules {
+    pub access_provider: Publishers,
+    pub ldap: Publishers,
+    pub hris: Publishers,
+    pub mozilliansorg: Publishers,
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
-#[serde(untagged)]
-pub enum StaffInformationRules {
-    Different {
-        manager: Publishers,
-        director: Publishers,
-        staff: Publishers,
-        title: Publishers,
-        team: Publishers,
-        cost_center: Publishers,
-        worker_type: Publishers,
-        wpr_desk_number: Publishers,
-        office_location: Publishers,
-    },
-    Same(Publishers),
+pub struct StaffInformationRules {
+    pub manager: Publishers,
+    pub director: Publishers,
+    pub staff: Publishers,
+    pub title: Publishers,
+    pub team: Publishers,
+    pub cost_center: Publishers,
+    pub worker_type: Publishers,
+    pub wpr_desk_number: Publishers,
+    pub office_location: Publishers,
 }
 
+impl FromStr for IdentityRules {
+    // This implementation of `from_str` can never fail, so use the impossible
+    // `Void` type as the error type.
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let publishers: Publishers = serde_json::from_value(s.into())?;
+        Ok(IdentityRules {
+            github_id_v3: publishers.clone(),
+            github_id_v4: publishers.clone(),
+            github_primary_email: publishers.clone(),
+            mozilliansorg_id: publishers.clone(),
+            bugzilla_mozilla_org_id: publishers.clone(),
+            bugzilla_mozilla_org_primary_email: publishers.clone(),
+            mozilla_ldap_id: publishers.clone(),
+            mozilla_ldap_primary_email: publishers.clone(),
+            mozilla_posix_id: publishers.clone(),
+            google_oauth2_id: publishers.clone(),
+            google_primary_email: publishers.clone(),
+            firefox_accounts_id: publishers.clone(),
+            firefox_accounts_primary_email: publishers.clone(),
+            custom_1_primary_email: publishers.clone(),
+            custom_2_primary_email: publishers.clone(),
+            custom_3_primary_email: publishers,
+        })
+    }
+}
+
+impl FromStr for AccessInformationRules {
+    // This implementation of `from_str` can never fail, so use the impossible
+    // `Void` type as the error type.
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let publishers: Publishers = serde_json::from_value(s.into())?;
+        Ok(AccessInformationRules {
+            access_provider: publishers.clone(),
+            ldap: publishers.clone(),
+            hris: publishers.clone(),
+            mozilliansorg: publishers,
+        })
+    }
+}
+
+impl FromStr for StaffInformationRules {
+    // This implementation of `from_str` can never fail, so use the impossible
+    // `Void` type as the error type.
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let publishers: Publishers = serde_json::from_value(s.into())?;
+        Ok(StaffInformationRules {
+            manager: publishers.clone(),
+            director: publishers.clone(),
+            staff: publishers.clone(),
+            title: publishers.clone(),
+            team: publishers.clone(),
+            cost_center: publishers.clone(),
+            worker_type: publishers.clone(),
+            wpr_desk_number: publishers.clone(),
+            office_location: publishers,
+        })
+    }
+}
+
+fn string_or_struct<'de, T, D>(deserializer: D) -> Result<T, D::Error>
+where
+    T: Deserialize<'de> + FromStr<Err = Error>,
+    D: Deserializer<'de>,
+{
+    // This is a Visitor that forwards string types to T's `FromStr` impl and
+    // forwards map types to T's `Deserialize` impl. The `PhantomData` is to
+    // keep the compiler from complaining about T being an unused generic type
+    // parameter. We need T in order to know the Value type for the Visitor
+    // impl.
+    struct StringOrStruct<T>(PhantomData<fn() -> T>);
+
+    impl<'de, T> Visitor<'de> for StringOrStruct<T>
+    where
+        T: Deserialize<'de> + FromStr<Err = Error>,
+    {
+        type Value = T;
+
+        fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+            formatter.write_str("string or map")
+        }
+
+        fn visit_str<E>(self, value: &str) -> Result<T, E>
+        where
+            E: de::Error,
+        {
+            Ok(FromStr::from_str(value).unwrap())
+        }
+
+        fn visit_map<M>(self, map: M) -> Result<T, M::Error>
+        where
+            M: MapAccess<'de>,
+        {
+            // `MapAccessDeserializer` is a wrapper that turns a `MapAccess`
+            // into a `Deserializer`, allowing it to be used as the input to T's
+            // `Deserialize` implementation. T then deserializes itself using
+            // the entries from the map visitor.
+            Deserialize::deserialize(de::value::MapAccessDeserializer::new(map))
+        }
+    }
+
+    deserializer.deserialize_any(StringOrStruct(PhantomData))
+}
 pub struct RemotePublisherRulesProvider {
     url: String,
 }
@@ -218,7 +324,8 @@ mod test {
 
     #[tokio::test]
     async fn test_remote() {
-        let rules = PUBLISHER_RULES.get().await;
+        let rules =
+            serde_json::from_str::<PublisherRules>(include_str!("../../tests/data/rules.json"));
         assert!(rules.is_ok());
     }
 }
--- src/profile/update.rs
+++ src/profile/update.rs
@@ -1,38 +1,102 @@
 use crate::error::ProfileError;
-use crate::profile::publishers::IdentityRules;
 use crate::profile::publishers::PUBLISHER_RULES;
+use cis_profile::schema::AccessInformationProviderSubObject;
 use cis_profile::schema::Profile;
+use cis_profile::schema::StandardAttributeBoolean;
 use cis_profile::schema::StandardAttributeString;
+use cis_profile::schema::StandardAttributeValues;
+
+pub enum Operation {
+    Create,
+    Update,
+    Noop,
+}
+
+macro_rules! update {
+    ($pf:ident, $uf:ident, $v:ident) => {{
+        if $pf.metadata.last_modified >= $uf.metadata.last_modified {
+            return Err(ProfileError::OutdatedUpdate);
+        }
+        Ok(())
+    }};
+}
 
 fn update_sas(
     field: &mut StandardAttributeString,
     update: StandardAttributeString,
 ) -> Result<(), ProfileError> {
-    if field.metadata.last_modified >= update.metadata.last_modified {
-        return Err(ProfileError::OutdatedUpdate);
-    }
-    Ok(())
+    update!(field, update, value)
+}
+fn update_sab(
+    field: &mut StandardAttributeBoolean,
+    update: StandardAttributeBoolean,
+) -> Result<(), ProfileError> {
+    update!(field, update, value)
+}
+
+fn update_sav(
+    field: &mut StandardAttributeValues,
+    update: StandardAttributeValues,
+) -> Result<(), ProfileError> {
+    update!(field, update, values)
+}
+
+fn update_saac(
+    field: &mut AccessInformationProviderSubObject,
+    update: AccessInformationProviderSubObject,
+) -> Result<(), ProfileError> {
+    update!(field, update, values)
+}
+
+macro_rules! update_allowed_any {
+    ($($f:ident).*, $p:ident, $u:ident, $s:ident, $v:ident, $c:ident) => {{
+        let op = if $p.$($f).* == $u.$($f).* || $u.$($f).*.$v.is_none() {
+            Ok(Operation::Noop)
+        } else {
+            if $p.$($f).*.$v.is_none() {
+                if !update_allowed!($p.$($f).*, $s.create.$($f).*) {
+                    Err(ProfileError::PublisherNotAllowedToCreate)
+                } else {
+                    Ok(Operation::Create)
+                }
+            } else {
+                if !update_allowed!($p.$($f).*, $s.update.$($f).*) {
+                    Err(ProfileError::PublisherNotAllowedToUpdate)
+                } else {
+                    Ok(Operation::Update)
+                }
+            }
+        };
+        match op {
+            Ok(Operation::Create) => $c(&mut $p.$($f).*, $u.$($f).*),
+            Ok(Operation::Update) => $c(&mut $p.$($f).*, $u.$($f).*),
+            Ok(Operation::Noop) => Ok(()),
+            Err(e) => Err(e),
+        }
+    }};
 }
 
 macro_rules! update_allowed_sas {
-    ($p:ident, $f:ident, $u:ident, $s:ident) => {
-        if $u.$f.value.is_some() {
-            if $p.$f.value.is_none() {
-                if !update_allowed!($p.$f, $s.create.$f) {
-                    Err(ProfileError::PublisherNotAllowedToCreate)
-                } else {
-                    update_sas(&mut $p.$f, $u.$f)
-                }
-            } else {
-                if !update_allowed!($p.$f, $s.update.$f) {
-                    Err(ProfileError::PublisherNotAllowedToUpdate)
-                } else {
-                    update_sas(&mut $p.$f, $u.$f)
-                }
-            }
-        } else {
-            Ok(())
-        }
+    ($($f:ident).*, $p:ident, $u:ident, $s:ident) => {
+        update_allowed_any!($($f).*, $p, $u, $s, value, update_sas)
+    };
+}
+
+macro_rules! update_allowed_sav {
+    ($($f:ident).*, $p:ident, $u:ident, $s:ident) => {
+        update_allowed_any!($($f).*, $p, $u, $s, values, update_sav)
+    };
+}
+
+macro_rules! update_allowed_sab {
+    ($($f:ident).*, $p:ident, $u:ident, $s:ident) => {
+        update_allowed_any!($($f).*, $p, $u, $s, value, update_sab)
+    };
+}
+
+macro_rules! update_allowed_saac {
+    ($($f:ident).*, $p:ident, $u:ident, $s:ident) => {
+        update_allowed_any!($($f).*, $p, $u, $s, values, update_saac)
     };
 }
 
@@ -41,45 +105,107 @@ macro_rules! update_allowed {
         $r.check(&$p.signature.publisher.name)
     };
 }
-macro_rules! update_allowed_identity {
-    ($p:expr, $r:expr, $i:ident) => {
-        match $r {
-            IdentityRules::Different { $i, .. } => $i.check(&$p.signature.publisher.name),
-            IdentityRules::Same(p) => p.check(&$p.signature.publisher.name),
-        }
-    };
-}
 
 pub async fn update(mut p: Profile, u: Profile) -> Result<Profile, ProfileError> {
     let rules = PUBLISHER_RULES.get().await.unwrap().rules;
-    update_allowed_sas!(p, primary_username, u, rules)?;
+    update_allowed_sas!(uuid, p, u, rules)?;
+    update_allowed_sas!(user_id, p, u, rules)?;
+    update_allowed_sas!(primary_username, p, u, rules)?;
+    update_allowed_sas!(login_method, p, u, rules)?;
+    update_allowed_sab!(active, p, u, rules)?;
+    update_allowed_sas!(last_modified, p, u, rules)?;
+    update_allowed_sas!(created, p, u, rules)?;
+    update_allowed_sav!(usernames, p, u, rules)?;
+    update_allowed_sas!(pronouns, p, u, rules)?;
+    update_allowed_sas!(first_name, p, u, rules)?;
+    update_allowed_sas!(last_name, p, u, rules)?;
+    update_allowed_sas!(alternative_name, p, u, rules)?;
+    update_allowed_sas!(primary_email, p, u, rules)?;
+    update_allowed_sav!(ssh_public_keys, p, u, rules)?;
+    update_allowed_sav!(pgp_public_keys, p, u, rules)?;
+    update_allowed_sas!(fun_title, p, u, rules)?;
+    update_allowed_sas!(description, p, u, rules)?;
+    update_allowed_sas!(location, p, u, rules)?;
+    update_allowed_sas!(timezone, p, u, rules)?;
+    update_allowed_sav!(languages, p, u, rules)?;
+    update_allowed_sav!(tags, p, u, rules)?;
+    update_allowed_sas!(picture, p, u, rules)?;
+    update_allowed_sav!(uris, p, u, rules)?;
+    update_allowed_sav!(phone_numbers, p, u, rules)?;
+
+    update_allowed_sas!(identities.github_id_v3, p, u, rules)?;
+    update_allowed_sas!(identities.github_id_v4, p, u, rules)?;
+    update_allowed_sas!(identities.github_primary_email, p, u, rules)?;
+    update_allowed_sas!(identities.mozilliansorg_id, p, u, rules)?;
+    update_allowed_sas!(identities.bugzilla_mozilla_org_id, p, u, rules)?;
+    update_allowed_sas!(identities.bugzilla_mozilla_org_primary_email, p, u, rules)?;
+    update_allowed_sas!(identities.mozilla_ldap_id, p, u, rules)?;
+    update_allowed_sas!(identities.mozilla_ldap_primary_email, p, u, rules)?;
+    update_allowed_sas!(identities.mozilla_posix_id, p, u, rules)?;
+    update_allowed_sas!(identities.google_oauth2_id, p, u, rules)?;
+    update_allowed_sas!(identities.google_primary_email, p, u, rules)?;
+    update_allowed_sas!(identities.firefox_accounts_id, p, u, rules)?;
+    update_allowed_sas!(identities.firefox_accounts_primary_email, p, u, rules)?;
+    update_allowed_sas!(identities.custom_1_primary_email, p, u, rules)?;
+    update_allowed_sas!(identities.custom_2_primary_email, p, u, rules)?;
+    update_allowed_sas!(identities.custom_3_primary_email, p, u, rules)?;
+
+    update_allowed_saac!(access_information.access_provider, p, u, rules)?;
+    update_allowed_saac!(access_information.ldap, p, u, rules)?;
+    update_allowed_saac!(access_information.hris, p, u, rules)?;
+    update_allowed_saac!(access_information.mozilliansorg, p, u, rules)?;
+
+    update_allowed_sab!(staff_information.manager, p, u, rules)?;
+    update_allowed_sab!(staff_information.director, p, u, rules)?;
+    update_allowed_sab!(staff_information.staff, p, u, rules)?;
+    update_allowed_sas!(staff_information.title, p, u, rules)?;
+    update_allowed_sas!(staff_information.team, p, u, rules)?;
+    update_allowed_sas!(staff_information.cost_center, p, u, rules)?;
+    update_allowed_sas!(staff_information.worker_type, p, u, rules)?;
+    update_allowed_sas!(staff_information.wpr_desk_number, p, u, rules)?;
+    update_allowed_sas!(staff_information.office_location, p, u, rules)?;
     Ok(p)
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
+    use crate::profile::publishers::PublisherRules;
     use chrono::Utc;
     use failure::Error;
 
     #[tokio::test]
     async fn test_rules() -> Result<(), Error> {
-        let rules = PUBLISHER_RULES.get().await.unwrap().rules;
+        let rules: PublisherRules =
+            serde_json::from_str(include_str!("../../tests/data/rules.json"))?;
         let mut p = Profile::default();
         let mut u = Profile::default();
         u.pronouns.value = Some(String::from("dino"));
         u.pronouns.metadata.last_modified = Utc::now();
         u.user_id.value = Some(String::from("dino"));
         u.user_id.metadata.last_modified = Utc::now();
+        u.identities.github_id_v3.value = Some(String::from("dino"));
+        u.identities.github_id_v3.metadata.last_modified = Utc::now();
         assert!(update_allowed!(p.pronouns, rules.update.pronouns));
-        assert!(update_allowed_identity!(
-            p.identities.github_id_v4,
-            rules.update.identities,
-            github_id_v4
-        ));
-        assert!(update_allowed_sas!(p, pronouns, u, rules).is_ok());
-        assert!(update_allowed_sas!(p, user_id, u, rules).is_err());
+        assert!(update_allowed_sas!(identities.github_id_v4, p, u, rules).is_ok());
+        assert!(
+            update_allowed_any!(identities.github_id_v3, p, u, rules, value, update_sas).is_ok()
+        );
+        assert!(update_allowed_sas!(pronouns, p, u, rules).is_ok());
+        assert!(update_allowed_sas!(user_id, p, u, rules).is_err());
         assert!(!update_allowed!(p.uuid, rules.update.uuid));
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_update() -> Result<(), Error> {
+        let p = Profile::default();
+        let mut u = Profile::default();
+        u.pronouns.value = Some(String::from("dino"));
+        u.pronouns.metadata.last_modified = Utc::now();
+        u.identities.github_id_v3.value = Some(String::from("dino"));
+        u.identities.github_id_v3.metadata.last_modified = Utc::now();
+        update(p, u).await?;
         Ok(())
     }
 }
